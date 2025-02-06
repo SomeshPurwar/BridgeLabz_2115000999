68c19a8d1ed71b612e332138a6d93419f4cc581a
SomeshPurwar <somesh.purwar20@gmail.com>
2025-02-06 22:50:04 +0530

    Assignment done on class and object Modelling on 06-02-2025

---
+++ Assignment18/BankandAccountHoldersAssociation/BankandAccountHoldersAssociation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using BankandAccountHoldersAssociation;
+
+class Program
+{
+    static void Main()
+    {
+        Bank bank1 = new Bank("PNB");
+        Bank bank2 = new Bank("SBI");
+
+        bank1.openAccount("Somesh", 1000);
+
+
+        Customer somesh = new Customer("Somesh", bank2, 1000);
+        Customer krishna = new Customer("Krishna", bank1, 2000);
+
+        somesh.viewBalance();
+        krishna.viewBalance();
+
+    }
+}
