5bd34cf06bcf1e06775c41a8d7b72fce45c2e818
SomeshPurwar <somesh.purwar20@gmail.com>
2025-02-22 12:48:24 +0530

    Assignment done on Streams(File Handling).

---
+++ Assignment28/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Program{
+    public static void Main()
+    {
+    //    Test1.Print();
+    //    Test2.Print();
+    //    Test3.Print();
+    //    Test4.Print();
+    //    Test5.Print();
+    //    Test6.Print();
+    //    Test7.Print();
+       Test8.Print();
+    //    Test9.Print();
+    //    Test10.Print();
+    }
+}
