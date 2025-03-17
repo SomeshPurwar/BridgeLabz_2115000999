37110ce969036757accb2bac9d4adbf51db2879c
SomeshPurwar <somesh.purwar20@gmail.com>
2025-03-17 15:30:10 +0530

    [Somesh] add:added Datatypes Javascript

---
+++ Datatypes.js
@@ -0,0 +1,40 @@
+// // Number
+
+// let n = 123; // integer
+// let m = 12.345; // floating
+// console.log(n);
+// console.log(m);
+// console.log(1/0); //Infinity
+// console.log(Infinity);
+// console.log("not a number" / 2) //NaN
+// console.log(NaN ** 0); // 1
+
+// // BigInt
+
+// console.log(9007199254740991 + 1); // 9007199254740992
+// console.log(9007199254740991 + 2); // 9007199254740992
+// const bigInt = 1234567890123456789012345678901234567890n;
+// console.log(bigInt);
+// console.log(typeof(bigInt));
+
+// // String
+
+// let str = "Hello";
+// let str2 = 'Single quotes are ok too';
+// let phrase = `can embed another ${str} ${str2}`;
+// console.log(phrase);
+
+// //Boolean
+
+// let nameFieldChecked = true; // yes, name field is checked
+// let ageFieldChecked = false; // no, age field is not checked
+// console.log(nameFieldChecked);
+// console.log(ageFieldChecked);
+
+// // null type
+// let age = null;
+// console.log(typeof(age));
+
+// undefined
+let age;
+console.log(typeof(age));
