6c9a99f4298ce693bb64999bdbe6be9e65f19bfe
SomeshPurwar <somesh.purwar20@gmail.com>
2025-03-17 14:56:45 +0530

    [Somesh] add:added Variables Javascript

---
+++ Variable.js
@@ -0,0 +1,17 @@
+// let message = 'Hello World!'; // define the variable and assign the value
+// console.log(message);
+// let user = 'John', age = 25, message = 'Hello'; //declare multiple variables in one line
+// console.log(`name:${user} age:${age} message:${message}`);
+
+// var message = 'Hello'; //In older scripts
+// console.log(message);
+
+let message;
+
+message = 'Hello!';
+
+message = 'World!'; // value changed
+console.log(message);
+
+const myBirthday = '18.04.1982'; //a constant (unchanging) variable
+console.log(myBirthday); 
